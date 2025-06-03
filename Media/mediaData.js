83bb06e7308ec2efc91aeeca3ea876cc33f36b26
Nurudeen Adedeji <djsmacker01@gmail.com>
2025-06-03 13:25:25 +0100

    commit

--- Media/mediaData.js
+++ Media/mediaData.js
@@ -99,7 +99,7 @@ const mediaData = {
         imageAlt:
           "Young participant experiencing the VR showcase of Welsh women's sports journey",
         source: {
-          //   logo: "/Images/klat.jpg",
+          logo: "/Images/klat.jpg",
           avatar: "/Images/seran.jpeg",
 
           name: "Voice.Wales",
