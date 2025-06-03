2930f7d41099f9a44803cc71fcbb4ada40fbb4a4
Nurudeen Adedeji <djsmacker01@gmail.com>
2025-06-03 12:47:07 +0100

    commit

--- Media/mediaData.js
+++ Media/mediaData.js
@@ -122,7 +122,7 @@ const mediaData = {
         tabId: "instagram",
         posts: [
           {
-            image: "Images/sportworkshop.jpg",
+            image: "/Images/sportworkshop.jpg",
             avatar: "Images/seran.jpeg",
             username: "@CymruUnleashed",
             timeAgo: "2 days ago",
